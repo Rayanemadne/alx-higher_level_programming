5637d6bf1e29a5349458e44f464cde2fec13b468
Rayanemadne <r.madoune@gmail.com>
2023-06-08 13:41:14 +0200

    script 100

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
+    """ my calculator """
     from calculator_1 import add, sub, mul, div
     import sys
+
     argc = len(sys.argv) -1
     if argc != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
