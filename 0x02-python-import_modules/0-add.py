75b82b2ed832cb22f7f8570007ed83ca9add7f6b
Rayanemadne <r.madoune@gmail.com>
2023-06-08 12:44:42 +0200

    script 0 update

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
+
 #RAYANE - MAD
 
 if __name__ == "__main__":
-    """ pritn addition """
+    """ print addition """
     from add_0 import add
-
     a = 1
     b = 2
-
     print("{0} + {1} = {2}".format(a, b, add(a, b)))
