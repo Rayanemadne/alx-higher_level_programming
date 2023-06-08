72b2b30368411a26898b8ac3447fcb24bffba561
Rayanemadne <r.madoune@gmail.com>
2023-06-08 13:04:02 +0200

    script 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -8,5 +8,5 @@ if __name__ == "__main__":
 
     sum = 0
     for i in range(len(sys.argv) - 1):
-        sum = sum + int(sys.argv[i + 1])
+        sum += int(sys.argv[i + 1])
     print("{}".format(sum))
