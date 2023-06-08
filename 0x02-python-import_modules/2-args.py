63116bd8d069558d4dcf2b6c3b85a4420dca4d31
Rayanemadne <r.madoune@gmail.com>
2023-06-08 12:42:37 +0200

    script 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 # RAYANE - MAD
-if __name__ == "__main__":
-    import sys
 
+if __name__ == "__main__":
+    """Print arguments"""
+    import sys
 
     n = len(sys.argv) - 1
     if n == 0:
