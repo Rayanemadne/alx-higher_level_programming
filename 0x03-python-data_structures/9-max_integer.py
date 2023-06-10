deddfc319a7e798989744e2f07b0c3ffb2f812dd
Rayanemadne <r.madoune@gmail.com>
2023-06-10 17:54:12 +0200

    script 9

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# RAYANE - MAD
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return "None"
+    else:
+        max = 0
+        for i in range(len(my_list) - 1):
+            if my_list[i] > my_list[i + 1]:
+                max = my_list[i]
+        return max
