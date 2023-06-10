2fff081a591afd4a5db2f0ab01dcb39e04ba8f4c
Rayanemadne <r.madoune@gmail.com>
2023-06-10 17:48:15 +0200

    script 8

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# RAYANE - MAD
+def multiple_returns(sentence):
+    tuple_return = ()
+    if len(sentence) == 0:
+        tuple_return = 0, "None"
+    else:
+        tuple_return = len(sentence), sentence[0]
+    return tuple_return
