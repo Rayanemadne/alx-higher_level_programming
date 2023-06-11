1a0cd10f88a47e67734dce971bd078a01352ea36
Rayanemadne <r.madoune@gmail.com>
2023-06-11 13:54:38 +0200

    script 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
 def no_c(my_string):
-    new_str = my_string.translate({ord(i): None for i in 'cC'})
-    return new_str
+    new_string = ""
+    for char in string:
+        if char.lower() not in ('c', 'C'):
+            new_string += char
+    return new_string
